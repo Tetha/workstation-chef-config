c561a818ef29ecb35461d362fccce12679b65d4b
Harald Kraemer <tetha42@gmail.com>
2015-02-20 21:23:23 +0100

    syntax

--- workstation_cookbook/attributes/default.rb
+++ workstation_cookbook/attributes/default.rb
@@ -6,17 +6,17 @@ default['my-workstation']['xterm']['packages'] = %w(xterm)
 default['my-workstation']['vim']['packages'] = %w(vim gvim)
 
 default['my-workstation']['vim']['pathogen-plugins']['easytags'] = {
-    repo => https://github.com/xolox/vim-easytags
+    repo => "https://github.com/xolox/vim-easytags"
 }
 
 default['my-workstation']['vim']['pathogen-plugins']['easymotion'] = {
-    repo => https://github.com/Lokaltog/vim-easymotion
+    repo => "https://github.com/Lokaltog/vim-easymotion"
 }
 
 default['my-workstation']['vim']['pathogen-plugins']['powerline'] = {
-    repo => https://github.com/Lokaltog/vim-powerline
+    repo => "https://github.com/Lokaltog/vim-powerline"
 }
 
 default['my-workstation']['vim']['pathogen-plugins']['localvimrc'] = {
-    repo => https://github.com/embear/vim-localvimrc
+    repo => "https://github.com/embear/vim-localvimrc"
 }
