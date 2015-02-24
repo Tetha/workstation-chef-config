fcd06be97b4e76025984392dcc26ee755c871f0a
Harald Kraemer <tetha42@gmail.com>
2015-02-24 21:54:57 +0100

    add i3 config

--- workstation_cookbook/recipes/i3.rb
+++ workstation_cookbook/recipes/i3.rb
@@ -4,3 +4,16 @@ node['my-workstation']['i3']['packages'].each do |p|
     action :upgrade
   end
 end
+
+directory "#{node['my-workstation']['home']}/.i3" do
+  action :create
+  owner node['my-workstation']['user']
+  group node['my-workstation']['group']
+  recursive true
+end
+
+template "#{node['my-workstation']['home']}/.i3/config" do
+  source "i3_config.erb"
+  owner node['my-workstation']['user']
+  group node['my-workstation']['group']
+end
