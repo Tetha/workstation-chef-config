6f443bb42cc769ff5f3a24d08c0ef6023f4261e4
Harald Kraemer <tetha42@gmail.com>
2015-02-24 21:42:52 +0100

    deploy xresources to stop my eyes from bleeding

--- workstation_cookbook/recipes/xterm.rb
+++ workstation_cookbook/recipes/xterm.rb
@@ -4,3 +4,18 @@ node['my-workstation']['xterm']['packages'].each do |p|
     action :upgrade
   end
 end
+
+template "#{node['my-workstation']['home']}/.Xresources" do
+  source 'Xresources.erb'
+  owner node['my-workstation']['user']
+  group node['my-workstation']['group']
+  notifies :run, 'bash[source_xresources]', :immediately
+end
+
+bash 'source_xresources' do
+  action :nothing
+  cwd node['my-workstation']['home']
+  code <<-EOH
+    xrdb -load ~/.Xresources
+    EOH
+end
