0c149089f7fd90948f9fa67b87f8f559e1d4a654
Harald Kraemer <tetha42@gmail.com>
2015-02-24 22:05:29 +0100

    Also change login shell

--- workstation_cookbook/recipes/zsh.rb
+++ workstation_cookbook/recipes/zsh.rb
@@ -1,4 +1,10 @@
 
+bash 'change login shell' do
+  cwd '/tmp'
+  code <<-EOH
+    chsh -s "${which zsh}" hkraemer
+  EOH
+end
 template "#{node['my-workstation']['home']}/.zshrc" do
   source 'zshrc.erb'
 end
