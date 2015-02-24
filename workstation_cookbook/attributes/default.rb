fc47d8c8dd471e2b925abfd826b055fc0fc33c03
Harald Kraemer <tetha42@gmail.com>
2015-02-24 22:09:14 +0100

    forgot attributes

--- workstation_cookbook/attributes/default.rb
+++ workstation_cookbook/attributes/default.rb
@@ -6,3 +6,4 @@ default['my-workstation']['group'] = 'hkraemer'
 
 default['my-workstation']['i3']['packages'] = %w(i3 i3lock i3status)
 default['my-workstation']['xterm']['packages'] = %w(xterm)
+default['my-workstation']['zsh']['packages'] = %w(zsh)
