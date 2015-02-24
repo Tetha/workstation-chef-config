144131e7a57583850abcec33830fbcb910156f6d
Harald Kraemer <tetha42@gmail.com>
2015-02-24 21:28:09 +0100

    Font installation

--- workstation_cookbook/recipes/powerline.rb
+++ workstation_cookbook/recipes/powerline.rb
@@ -9,3 +9,24 @@ execute 'Install Powerline' do
   command 'pip install powerline-status'
   creates '/usr/bin/powerline'
 end
+
+directory node['my-workstation']['cache'] do
+  action :create
+  owner node['my-workstation']['user']
+  group node['my-workstation']['group']
+  recursive true
+end
+
+git clone 'https://github.com/powerline/fonts.git' do
+  action :checkout
+  destination "#{node['my-workstation']['cache']}/powerline_fonts"
+  notifies :run, 'bash[install_fonts]', :immediately
+end
+
+bash 'install_fonts' do
+  action :nothing
+  cwd "#{node['my-workstation']['cache']}/powerline_fonts"
+  code <<-EOH
+    ./install.sh
+    EOH
+end
