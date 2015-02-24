79409ec1404b0913182523f4d7eb8c2372de8749
Harald Kraemer <tetha42@gmail.com>
2015-02-24 21:56:40 +0100

    Add i3status

--- workstation_cookbook/attributes/default.rb
+++ workstation_cookbook/attributes/default.rb
@@ -4,5 +4,5 @@ default['my-workstation']['cache'] = '/home/hkraemer/.workstation_chef_tmp'
 default['my-workstation']['user'] = 'hkraemer'
 default['my-workstation']['group'] = 'hkraemer'
 
-default['my-workstation']['i3']['packages'] = %w(i3 i3lock)
+default['my-workstation']['i3']['packages'] = %w(i3 i3lock i3status)
 default['my-workstation']['xterm']['packages'] = %w(xterm)
