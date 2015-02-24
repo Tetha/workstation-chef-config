3845371b67dc39f8e134ae503729d6eaf9e27111
Harald Kraemer <tetha42@gmail.com>
2015-02-24 22:21:11 +0100

    require qgit

--- workstation_cookbook/attributes/default.rb
+++ workstation_cookbook/attributes/default.rb
@@ -4,6 +4,9 @@ default['my-workstation']['cache'] = '/home/hkraemer/.workstation_chef_tmp'
 default['my-workstation']['user'] = 'hkraemer'
 default['my-workstation']['group'] = 'hkraemer'
 
+# TODO: merge all of these into packages, no use making this
+# flexible.
+default['my-workstation']['packages'] = %(qgit)
 default['my-workstation']['i3']['packages'] = %w(i3 i3lock i3status)
 default['my-workstation']['xterm']['packages'] = %w(xterm)
 default['my-workstation']['zsh']['packages'] = %w(zsh)
